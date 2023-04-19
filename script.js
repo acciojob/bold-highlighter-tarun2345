870e766dfdd15be55439e8cd7594445a2c65ae9b
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 19:28:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function highlight() {
     //Write your code here
-
-
+	bold.style.color = green;
 }
 
+let bold = document.getElementsByTagName("strong");
 
 function return_normal() {
     //Write your code here
 
-    
+    bold.style.color = black;
 }
